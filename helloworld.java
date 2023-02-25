7c8df277cfa3baf79195bbdb4b3f49c47165bb45
Sanket Patil <124522090+sanketp099@users.noreply.github.com>
2023-02-25 19:37:39 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 25th 2023 - 19:35 - cron trigger");
+    System.out.println("Hello World - Feb 25th 2023 - 19:37 - cron trigger");
     }
 }
